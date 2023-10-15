7fcde0a49a352e8b9d06929e2b1bdf64011b716c
aquibbaig <aquibbaig97@gmail.com>
2023-10-15 11:53:18 +0530

    theme context and prettier setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,26 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      "background": {
+        "primary": {
+          "light": "#F9FAFC",
+          "dark": "#101010"
+        },
+      },
+      "text": {
+        "primary": {
+          "light": "#333333",
+          "dark": "#E0E0E0"
+        },
+      },
+      "border": {
+        "primary": {
+          "light": "#EEF1F4",
+          "dark": "#202020"
+        },
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
