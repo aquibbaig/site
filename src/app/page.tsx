94056ae730ecbb831c40fde8b41ea3edcb644d94
aquibbaig <aquibbaig97@gmail.com>
2023-10-17 00:19:06 +0530

    add bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-y-20">
+      <div>
+        Passionate about creating products that address real-world challenges, I strive to excel in
+        developing highly performant and maintainable systems. I have a strong affinity for
+        aesthetically pleasing designs and a keen eye for exceptional user experiences.
+      </div>
+    </div>
+  );
 }
--- src/services/page-header/PageHeader.tsx
+++ src/services/page-header/PageHeader.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitch } from "../theme/ThemeSwitch";
 
 export const PageHeader = () => {
   return (
-    <div className="flex flex-row justify-between items-center">
+    <div className="flex flex-row justify-between items-center mb-20">
       <Logo />
       <ThemeSwitch />
     </div>
