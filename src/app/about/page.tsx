185a26b6d7f582de46ba900232c8c5ece8ec65ac
aquibbaig <aquibbaig97@gmail.com>
2024-01-30 22:17:06 +0530

    adds about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import Head from 'next/head';
+import { externalLinkCSS } from '../_lib/helpers';
 
 export const metadata: Metadata = {
   title: 'About | Aquib Baig',
@@ -13,6 +14,32 @@ export default function About() {
         <title>About | Aquib Baig</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <div className="flex flex-col gap-y-4">
+        <h5 className="text-text-muted-light dark:text-text-muted-dark">About</h5>
+        <div>
+          <p>
+            I am a frontend engineer based in Bangalore, India. I work closely with designers and
+            product managers to build products that are accessible, performant, and delightful to
+            use. I am passionate about building high-quality software that is scalable and
+            maintainable.
+          </p>
+          <p className="text-text-muted-light dark:text-text-muted-dark">
+            I keep a keen interest in capturing the best parts of life and sharing them with the
+            world. I love to write about things I have learned and moments that make me think. I
+            believe in doing things my way, not just following what everyone else is doing.
+          </p>
+        </div>
+        <p>
+          Currently at{' '}
+          <a href="https://www.toplyne.io/" target="_blank" className={externalLinkCSS}>
+            Toplyne
+          </a>
+          , previously at{' '}
+          <a href="https://www.redhat.com/en" target="_blank" className={externalLinkCSS}>
+            Redhat
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
