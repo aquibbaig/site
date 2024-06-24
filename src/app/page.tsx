52f82cbaca4e1052597cb5bdbb01b3f135d412cb
aquibbaig <aquibbaig97@gmail.com>
2024-06-24 16:09:31 +0530

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import { POST_PREVIEW_LIMIT } from '@/constants';
-import { getPostMetadata } from '@/helpers';
 import type { Metadata } from 'next';
 import { Browse } from './_lib/Browse';
 import { Socials } from './_lib/Socials';
-import { getPost } from './blog/[slug]/page';
 
 export const metadata: Metadata = {
   title: 'Aquib Baig',
   description: 'Frontend Engineer',
 };
 
-export default async function Home() {
-  const { recentPosts } = await getRecentPosts();
-
+export default function Home() {
   return (
     <>
       <p>
@@ -22,47 +17,7 @@ export default async function Home() {
         work.
       </p>
       <Browse />
-      {/* <div className="flex flex-col gap-y-2">
-        <Link href="/blog">
-          <h4>Writings</h4>
-        </Link>
-        <ul className="flex flex-col gap-y-2">
-          {recentPosts.map(({ title, slug }) => (
-            <li key={slug}>
-              <Link as={`/blog/${slug}`} href={`/posts/[slug]`} className={twMerge('flex')}>
-                <p className="truncate">{title}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div> */}
       <Socials />
     </>
   );
-}
-
-async function getRecentPosts() {
-  const { posts = [], count } = getPostMetadata({
-    limit: POST_PREVIEW_LIMIT,
-  });
-
-  let recentPosts: {
-    title: string;
-    slug: string;
-  }[] = [];
-
-  for (const post of posts) {
-    const slug = post.filePath.replace(/\.mdx?$/, '');
-    const { frontmatter } = await getPost({ slug });
-
-    recentPosts.push({
-      title: frontmatter.title,
-      slug,
-    });
-  }
-
-  return {
-    recentPosts,
-    totalCount: count,
-  };
 }
