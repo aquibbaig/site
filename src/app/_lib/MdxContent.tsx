7db310c76d4f7349a8c35ea6bf09c3bb0e2bb01e
Aquib Baig <aquibbaig@styKid.local>
2024-07-15 23:51:06 +0530

    add shiki as syntax highlighter

--- src/app/_lib/MdxContent.tsx
+++ src/app/_lib/MdxContent.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { Code } from '../blog/[slug]/_lib/Code';
 
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
 };
 
-const MdxComponents = {};
+const MdxComponents = {
+  pre: (props: any) => Code(props),
+};
 
 export function MdxContent({ source }: MdxContentProps) {
   return <MDXRemote {...source} components={MdxComponents} />;
