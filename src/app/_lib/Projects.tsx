7c3ba035e83697f49b527f5060b587c47c8d07c3
Aquib Baig <aquibbaig97@gmail.com>
2024-12-13 20:53:48 +0530

    do not use shadcn components while animating

--- src/app/_lib/Projects.tsx
+++ src/app/_lib/Projects.tsx
@@ -2,77 +2,97 @@
 
 import BenchRoutesLogo from '@/assets/bench-routes.png';
 import EnviseLogo from '@/assets/envise.png';
-import { Dialog, DialogContent } from '@/components/ui/Dialog';
 import { ProjectType } from '@/constants';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 import PlugZap from 'lucide-static/icons/plug-zap.svg';
 import { AnimatePresence, motion } from 'motion/react';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const Projects = () => {
   const [projectDetails, showProjectDetails] = useState<ProjectType | null>(null);
 
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => showProjectDetails(null));
+
   return (
     <div>
       {Boolean(projectDetails) && (
-        <Dialog
-          open
-          onOpenChange={(open) => {
-            if (!open) {
-              showProjectDetails(null);
-            }
-          }}
-        >
-          <AnimatePresence>
-            <DialogContent className="pb-16 !rounded-2xl">
-              <motion.div layoutId={`project-${projectDetails?.uri}`}>
-                <motion.div
-                  className={twMerge(
-                    getGradientForProject(projectDetails?.id!),
-                    'p-2 rounded-xl size-14 md:size-12 flex flex-col items-center justify-center shrink-0'
-                  )}
-                  layoutId={`image-${projectDetails?.uri}`}
-                >
-                  <Image src={projectDetails?.icon} alt="icon" />
-                </motion.div>
-                <div className="flex justify-between w-full items-center h-full">
-                  <div className="flex flex-col w-[80%]">
-                    <motion.span
-                      layoutId={`title-${projectDetails?.uri}`}
-                      className="text-sm md:text-[15px]"
-                    >
-                      {projectDetails?.title}
-                    </motion.span>
-                    <motion.span
-                      layoutId={`alt-${projectDetails?.uri}`}
-                      className="text-sm md:text-[15px] text-text-secondary-light dark:text-text-secondary-dark"
-                    >
-                      {projectDetails?.description}
-                    </motion.span>
-                  </div>
-                  <div className="flex flex-row gap-4 items-center bg-background-secondary-light dark:bg-background-secondary-dark px-2 py-0.5 size-fit rounded-lg">
-                    <a
-                      href={projectDetails?.uri || '#'}
-                      target="_blank"
-                      className={twMerge(
-                        'w-full font-medium text-xs text-[#007AFE] dark:text-[#007AFE]'
-                      )}
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                    >
-                      Get
-                    </a>
-                  </div>
-                </div>
-              </motion.div>
-            </DialogContent>
-          </AnimatePresence>
-        </Dialog>
+        <AnimatePresence>
+          <motion.div
+            className="fixed inset-0 z-50 bg-black/80"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+          />
+        </AnimatePresence>
       )}
+      <AnimatePresence>
+        {Boolean(projectDetails) ? (
+          <motion.div
+            layoutId={`project-${projectDetails?.id}`}
+            className="fixed mx-4 outline-none z-50 -ml-4 grid w-full max-w-lg gap-4 border border-border-primary-light dark:border-border-primary-dark bg-background-primary-light dark:bg-background-primary-dark p-6 shadow-lg sm:rounded-xl"
+            ref={ref}
+          >
+            <motion.div
+              className={twMerge(
+                getGradientForProject(projectDetails?.id!),
+                'p-2 rounded-xl size-14 md:size-12 flex flex-col items-center justify-center shrink-0'
+              )}
+              layoutId={`image-${projectDetails?.id}`}
+            >
+              <Image src={projectDetails?.icon} alt="icon" />
+            </motion.div>
+            <div className="flex justify-between w-full items-center h-full">
+              <div className="flex flex-col">
+                <motion.span
+                  layoutId={`title-${projectDetails?.id}`}
+                  className="text-sm md:text-[15px] w-[80%]"
+                >
+                  {projectDetails?.title}
+                </motion.span>
+                <motion.span
+                  layoutId={`alt-${projectDetails?.id}`}
+                  className="w-[80%] text-sm md:text-[15px] text-text-secondary-light dark:text-text-secondary-dark"
+                >
+                  {projectDetails?.subHeading}
+                </motion.span>
+                <span className="mt-4 text-sm md:text-[15px] text-text-muted-light dark:text-text-muted-dark">
+                  {projectDetails?.description}
+                </span>
+              </div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="flex flex-row -mt-16 gap-4 items-center bg-background-secondary-light dark:bg-background-secondary-dark px-2 py-0.5 size-fit rounded-lg"
+              >
+                <a
+                  href={projectDetails?.uri || '#'}
+                  target="_blank"
+                  className={twMerge(
+                    'w-full font-medium text-xs text-[#007AFE] dark:text-[#007AFE]'
+                  )}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  Get
+                </a>
+              </motion.div>
+            </div>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
       <h4>Projects</h4>
-      <ul className="flex flex-col">
+      <div className="flex flex-col">
         {projects.map((project) => {
           return (
             <Project
@@ -82,7 +102,7 @@ export const Projects = () => {
             />
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
@@ -94,31 +114,30 @@ const Project = ({
   const { uri, id, icon, subHeading, title } = project;
 
   return (
-    <motion.li
-      key={uri}
+    <motion.div
       className={twMerge(
         'h-20 flex flex-row items-center gap-4 w-full cursor-pointer',
         'leading-relaxed'
       )}
       onClick={() => showDetails(project)}
-      layoutId={`project-${uri}`}
+      layoutId={`project-${id}`}
     >
       <motion.div
         className={twMerge(
           getGradientForProject(id),
           'p-2 rounded-xl size-14 md:size-12 flex flex-col items-center justify-center shrink-0'
         )}
-        layoutId={`image-${uri}`}
+        layoutId={`image-${id}`}
       >
         <Image src={icon} alt="icon" />
       </motion.div>
       <div className="flex justify-between w-full border-b border-border-primary-light/70 dark:border-border-primary-dark items-center h-full">
         <div className="flex flex-col w-[80%]">
-          <motion.span layoutId={`title-${uri}`} className="text-sm md:text-[15px]">
+          <motion.span layoutId={`title-${id}`} className="text-sm md:text-[15px]">
             {title}
           </motion.span>
           <motion.span
-            layoutId={`alt-${uri}`}
+            layoutId={`alt-${id}`}
             className="text-sm md:text-[15px] text-text-secondary-light dark:text-text-secondary-dark"
           >
             {subHeading}
@@ -137,7 +156,7 @@ const Project = ({
           </a>
         </div>
       </div>
-    </motion.li>
+    </motion.div>
   );
 };
 
@@ -159,7 +178,7 @@ const projects: ProjectType[] = [
     id: 'envise',
     title: 'Envise',
     uri: 'https://envise.app',
-    subHeading: 'A note taking app to help reflect and memorize.',
+    subHeading: 'A note taking app to help reflect and memorize',
     description: `Envise is note-taking app that helps you reflect on ideas and retain them for the long term.`,
     icon: EnviseLogo,
     size: [48, 48],
@@ -168,8 +187,8 @@ const projects: ProjectType[] = [
     id: 'spotify-connect',
     title: 'Spotify Connect',
     uri: 'https://www.npmjs.com/package/spotify-connect',
-    subHeading: 'Integrate spotify APIs in your web applications.',
-    description: `An npm package to implement spotify APIs in your web applications.`,
+    subHeading: 'Integrate spotify APIs in your web applications',
+    description: `spotify-connect is an npm package to implement spotify APIs directly in your web applications.`,
     icon: PlugZap,
     size: [48, 48],
   },
@@ -177,8 +196,8 @@ const projects: ProjectType[] = [
     id: 'bench-routes',
     title: 'Bench Routes',
     uri: 'https://github.com/bench-routes/bench-routes',
-    subHeading: 'Open source API monitoring tool.',
-    description: `A highly scalable API monitoring and performance analysis tool.`,
+    subHeading: 'Open source API monitoring tool',
+    description: `Bench routes is a highly scalable API monitoring and performance analysis tool written in Go.`,
     icon: BenchRoutesLogo,
     size: [48, 48],
   },
--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -90,41 +90,37 @@ const DialogDescription = React.forwardRef<
 ));
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
-const overlayVariants = {
-  closed: { opacity: 0 },
-  open: { opacity: 1 },
-};
 const AnimationDialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogPrimitive.Overlay
-      ref={ref}
-      className={cn(
-        'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-        className
-      )}
-      {...props}
-    >
-      <motion.div variants={overlayVariants} initial="closed" animate="open" exit="closed" />
-    </DialogPrimitive.Overlay>
-    <DialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        'fixed left-[50%] focus:outline-none top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border-primary-light dark:border-border-primary-dark bg-background-primary-light dark:bg-background-primary-dark p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-border-primary-light dark:ring-offset-border-primary-dark transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-border-primary-light dark:focus:ring-border-primary-dark focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-background-primary-light dark:data-[state=open]:bg-background-primary-dark data-[state=open]:text-text-muted-light dark:data-[state=open]:text-text-muted-dark">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
-    </DialogPrimitive.Content>
-  </DialogPortal>
-));
+>(({ className, children, ...props }, ref) => {
+  return (
+    <>
+      <motion.div
+        className="fixed inset-0 z-50 bg-black/80"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        exit={{
+          opacity: 0,
+        }}
+      />
+      <DialogPrimitive.Content
+        ref={ref}
+        className={cn(
+          'fixed outline-none z-50 grid w-full max-w-lg  gap-4 border border-border-primary-light dark:border-border-primary-dark bg-background-primary-light dark:bg-background-primary-dark p-6 shadow-lg sm:rounded-lg',
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </DialogPrimitive.Content>
+    </>
+  );
+});
 AnimationDialogContent.displayName = `AnimationDialogContent`;
 
 export {
