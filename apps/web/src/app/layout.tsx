0fd7ebcccaad020dfc10174904c68cb45dbdc461
Aquib Baig <aquibbaig97@gmail.com>
2025-08-06 17:31:56 +0530

    add geist

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,40 +13,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import localFont from 'next/font/local';
 import { SpotifyConnectContextProvider } from 'spotify-connect';
 import { PHProvider } from './providers';
-
-const soehne = localFont({
-  src: [
-    {
-      path: '../../public/fonts/test-soehne-extraleicht.woff2',
-      weight: '200',
-    },
-    {
-      path: '../../public/fonts/test-soehne-leicht.woff2',
-      weight: '300',
-    },
-    {
-      path: '../../public/fonts/test-soehne-buch.woff2',
-      weight: '400',
-    },
-    {
-      path: '../../public/fonts/test-soehne-kraftig.woff2',
-      weight: '500',
-    },
-    {
-      path: '../../public/fonts/test-soehne-halbfett.woff2',
-      weight: '600',
-    },
-    {
-      path: '../../public/fonts/test-soehne-dreiviertelfett.woff2',
-      weight: '700',
-    },
-    {
-      path: '../../public/fonts/test-soehne-fett.woff2',
-      weight: '800',
-    },
-  ],
-  variable: '--font-soehne',
-});
+import { GeistMono } from 'geist/font/mono';
 
 export const dynamic = 'force-static';
 
@@ -66,7 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 twMerge(
                   'bg-background-primary-light text-text-primary-light',
                   'dark:text-text-primary-dark dark:bg-background-primary-dark',
-                  `${soehne.variable} font-sans`
+                  GeistMono.className
                 )
               )}
             >
