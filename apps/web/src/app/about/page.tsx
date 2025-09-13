8ff18119db5a5d6a4fb0a2b7eb620bf9e06cbbb8
Aquib Baig <aquibbaig97@gmail.com>
2025-09-13 22:33:29 +0530

    #image

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'About | Aquib Baig',
@@ -14,10 +15,19 @@ export default function About() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-col gap-y-8">
-        <div className="flex flex-col gap-2">
-          <p>
-            When {`I'm`} not working, I love listening to music, playing football or Counter strike.
-          </p>
+        <div className="flex flex-col items-start gap-6">
+          <Image
+            src="/logo.jpg"
+            alt="Aquib Baig"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <div className="flex flex-col gap-2">
+            <p>
+              When {`I'm`} not working, I love listening to music, playing football or Counter strike.
+            </p>
+          </div>
         </div>
         <div className="flex flex-col gap-2">
           <h5 className="text-muted-foreground">Experience</h5>
