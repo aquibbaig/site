b913f453391313121c8de2397de72e4af043cca8
Aquib Baig <aquibbaig97@gmail.com>
2026-09-10 12:07:20 +0530

    🐛 fix(layout): unify all site pages to 688px content width with 32px gutters

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -58,9 +58,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               >
                 <main
                   className={twMerge(
-                    'max-w-[576px] mx-auto antialiased flex flex-col gap-8',
-                    'py-12 md:py-16 p-2',
-                    'has-[>.blog-post]:max-w-[752px] has-[>.blog-post]:px-8'
+                    'max-w-[752px] mx-auto antialiased flex flex-col gap-8',
+                    'py-12 md:py-16 px-8'
                   )}
                 >
                   <PageHeader />
